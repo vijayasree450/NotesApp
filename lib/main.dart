36bd9e629e986a9f3d0dffbe2a587b729a75a7c7
vijayasree450 <vijayasree450@gmail.com>
2026-05-21 15:21:50 +0530

    add update lib/main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +10,251 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: NotesPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class NotesPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<NotesPage> createState() => _NotesPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _NotesPageState extends State<NotesPage> {
 
-  void _incrementCounter() {
+  TextEditingController noteController =
+      TextEditingController();
+
+  List<String> notes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadNotes();
+  }
+
+  // ADD NOTE
+  void addNote() {
+    if (noteController.text.isEmpty) return;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      notes.add(noteController.text);
     });
+
+    noteController.clear();
+
+    saveNotes();
+  }
+
+  // DELETE NOTE
+  void deleteNote(int index) {
+    setState(() {
+      notes.removeAt(index);
+    });
+
+    saveNotes();
+  }
+
+  // EDIT NOTE
+  void editNote(int index) {
+
+    noteController.text = notes[index];
+
+    showDialog(
+      context: context,
+
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Edit Note"),
+
+          content: TextField(
+            controller: noteController,
+          ),
+
+          actions: [
+
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+
+              child: const Text("Cancel"),
+            ),
+
+            TextButton(
+              onPressed: () {
+
+                setState(() {
+                  notes[index] =
+                      noteController.text;
+                });
+
+                saveNotes();
+
+                noteController.clear();
+
+                Navigator.pop(context);
+              },
+
+              child: const Text("Save"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // SAVE NOTES
+  Future<void> saveNotes() async {
+
+    final prefs =
+        await SharedPreferences.getInstance();
+
+    String data = jsonEncode(notes);
+
+    await prefs.setString("notes", data);
+  }
+
+  // LOAD NOTES
+  Future<void> loadNotes() async {
+
+    final prefs =
+        await SharedPreferences.getInstance();
+
+    String? data =
+        prefs.getString("notes");
+
+    if (data != null) {
+
+      setState(() {
+
+        notes =
+            List<String>.from(
+              jsonDecode(data),
+            );
+
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
+
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Notes App"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+
+      floatingActionButton:
+          FloatingActionButton(
+
+        onPressed: () {
+
+          noteController.clear();
+
+          showDialog(
+            context: context,
+
+            builder: (context) {
+
+              return AlertDialog(
+
+                title: const Text("Add Note"),
+
+                content: TextField(
+                  controller: noteController,
+
+                  decoration:
+                      const InputDecoration(
+                    hintText: "Enter note",
+                  ),
+                ),
+
+                actions: [
+
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+
+                    child: const Text("Cancel"),
+                  ),
+
+                  TextButton(
+                    onPressed: () {
+
+                      addNote();
+
+                      Navigator.pop(context);
+                    },
+
+                    child: const Text("Add"),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+
         child: const Icon(Icons.add),
       ),
+
+      body: notes.isEmpty
+
+          ? const Center(
+              child: Text("No Notes Yet"),
+            )
+
+          : ListView.builder(
+
+              itemCount: notes.length,
+
+              itemBuilder: (context, index) {
+
+                return Card(
+
+                  margin: const EdgeInsets.all(10),
+
+                  child: ListTile(
+
+                    title: Text(notes[index]),
+
+                    trailing: Row(
+                      mainAxisSize:
+                          MainAxisSize.min,
+
+                      children: [
+
+                        IconButton(
+
+                          icon:
+                              const Icon(Icons.edit),
+
+                          onPressed: () {
+                            editNote(index);
+                          },
+                        ),
+
+                        IconButton(
+
+                          icon:
+                              const Icon(Icons.delete),
+
+                          onPressed: () {
+                            deleteNote(index);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
